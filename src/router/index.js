71ce4f283f292bd8a68cb7ec74c62d34518b2077
okto2000 <oktorino2000@gmail.com>
2024-09-27 16:41:32 +0700

    first commit

--- src/router/index.js
+++ src/router/index.js
@@ -4,10 +4,6 @@ import UserLayout from '@/components/layout/UserLayout.vue'
 
 // views for User layout
 import HomeView from '@/views/HomeView.vue'
-// import History from '@/views/user/History.vue'
-import Profile from '@/views/user/Profile.vue'
-import ShoppingCart from '@/views/user/ShoppingCart.vue'
-import DashboardUser from '@/views/user/Dashboard.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -21,30 +17,12 @@ const router = createRouter({
         name: 'home',
         component: HomeView
       },
-      {
-
-        path: '/shoppingcart',
-        name: 'shopping cart',
-        component: ShoppingCart
-      },
-      {
-
-        path: '/profile',
-        name: 'profile',
-        component: Profile
-      },
       // {
 
       //   path: '/riwayatpesanan',
       //   name: 'riwayatpesanan',
       //   component: History
       // },
-      {
-
-        path: '/dashboard',
-        name: 'dashboard-user',
-        component: DashboardUser
-      }      
       ]
     },
   ]
